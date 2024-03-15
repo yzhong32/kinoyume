f4b9d02957753b86ffce3de3371bc72936d05801
yzhong32 <yzhong32@illinois.edu>
2024-03-15 14:13:50 -0500

    adjust footer/logo

--- src/components/footer/logo/Logo.jsx
+++ src/components/footer/logo/Logo.jsx
@@ -1,63 +1,26 @@
-import styled from 'styled-components';
 import logo from './logo.png';
 import iconYelp from './icon-yelp.png';
 import iconInstagram from './icon-instagram.png';
 import iconFacebook from './icon-facebook.png';
-
-const PageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  box-sizing: border-box;
-  z-index: 2;
-  gap: 10px;
-`;
-
-const Logo = styled.img`
-  padding-top: 100px;
-  width: 100%;
-  margin-bottom: 0;
-  z-index: 2;
-`;
-
-const SocialMediaIcons = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 20px; 
-  margin-bottom: 20px;
-`;
-
-const Icon = styled.img`
-  width: 80px;
-`;
-
-const BottomBorder = styled.div`
-  width: 100%;
-  max-width: 300px;
-  height: 2px;
-  background-color: #aaa;
-  margin-bottom: -40px;
-`;
+import * as Styled from './styles'
 
 function LogoComponent() {
     return (
-        <PageContainer>
-            <Logo src={logo} alt="Kinoyume Logo" />
-            <SocialMediaIcons>
+        <Styled.LogoContainer>
+            <Styled.Logo src={logo} alt="Kinoyume Logo" />
+            <Styled.SocialMediaIcons>
                 <a href="https://www.yelp.com" target="_blank" rel="noopener noreferrer">
-                    <Icon src={iconYelp} alt="Yelp" />
+                    <Styled.Icon src={iconYelp} alt="Yelp" />
                 </a>
                 <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-                    <Icon src={iconInstagram} alt="Instagram" />
+                    <Styled.Icon src={iconInstagram} alt="Instagram" />
                 </a>
                 <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                    <Icon src={iconFacebook} alt="Facebook" />
+                    <Styled.Icon src={iconFacebook} alt="Facebook" />
                 </a>
-            </SocialMediaIcons>
-            <BottomBorder />
-        </PageContainer>
+            </Styled.SocialMediaIcons>
+            <Styled.BottomBorder />
+        </Styled.LogoContainer>
     );
 }
 
---
+++ src/components/footer/logo/styles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  z-index: 2;
+`;
+
+export const Logo = styled.img`
+    margin-bottom: 25.32px;
+`;
+
+export const SocialMediaIcons = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+  width: 252.66px;
+  height: 63px;
+  top: 111px;
+  margin-bottom: 24.75px;
+`;
+
+export const Icon = styled.img`
+`;
+
+export const BottomBorder = styled.div`
+  width: 320.36px;
+  height: 1px;
+  background-color: #CFCFCF;
+  background-image: linear-gradient(to right, #FFFFFF 0%, #CFCFCF 50%, #FFFFFF 100%);
+`;
